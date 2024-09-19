ed36d7b29f089099971ee382aadbfc276baa8692
priya <priya@priyanka>
2024-09-19 10:05:03 +0530

    first

---
+++ ComsandECommFramework/src/test/java/com/comsand/ecomm/LoginTest.java
@@ -0,0 +1,15 @@
+package com.comsand.ecomm;
+
+import org.testng.annotations.Test;
+
+import com.shoppoint.genericutility.Baseclass;
+
+public class LoginTest extends Baseclass{
+	
+	@Test
+	public void loginAsUserTest() {
+		
+		
+	}
+
+}
